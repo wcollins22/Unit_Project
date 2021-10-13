81c24b1cf4308b36aec17a2e92f4853e4c6da386
wcollins22 <89153002+wcollins22@users.noreply.github.com>
2021-10-13 11:24:38 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,7 @@
 from django import forms
 from app.models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["name", 'bio', 'dream', 'job', 'hometown']
