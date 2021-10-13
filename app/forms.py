51780b6dd855d95bac9a84d5aad7f264f62b242f
JustinAshmore22 <88673704+JustinAshmore22@users.noreply.github.com>
2021-10-13 13:15:16 -0500

    Co-authored-by: LoganColey <LoganColey@users.noreply.github.com> Co-authored-by: wcollins22 <wcollins22@users.noreply.github.com>

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,4 @@ from app.models import Student
 
 class ProfileForm(forms.Form):
     name = forms.CharField(max_length=50)
-    bio = forms.CharField(max_length=100)
-    dream = forms.CharField(max_length=100)
-    job = forms.CharField(max_length=50)
-    hometown = forms.CharField(max_length=50)
+   
