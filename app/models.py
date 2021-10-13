7339e8e935434f67c76308d3e5ad7d5cd5f93045
wcollins22 <89153002+wcollins22@users.noreply.github.com>
2021-10-13 13:14:22 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=50)
     bio = models.CharField(max_length=100)
+    fav_genre = models.CharField(max_length=50)
     dream = models.CharField(max_length=100)
     job = models.CharField(max_length=50)
     hometown = models.CharField(max_length=50)
