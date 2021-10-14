95f518c295c6d38656e5b5dd414e0fa1b8274617
LoganColey <lcoley22@basecampcodingacademy.org>
2021-10-14 13:01:40 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 import app.views
-from app.views import profile_function, homepage
+from app.views import profile_function, homepage_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("profile/", app.views.profile_function, name="pf"),
-    path("homepage/", app.views.homepage, name="hpnew")
+    path("homepage/", app.views.homepage_view, name="hpnew")
 ]
