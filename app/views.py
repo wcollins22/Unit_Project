2524c10a0892e832e71a8aad8f5f9b1d26248a0e
LoganColey <lcoley22@basecampcodingacademy.org>
2021-10-14 13:01:35 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.http.request import HttpRequest
-from app.forms import ProfileForm
+from app.forms import ProfileForm, SearchForm
 from dataclasses import dataclass
 
 @dataclass
@@ -126,9 +126,8 @@ def home(request,u_name):
     
     return render(request, "page.html", context)
 
-def homepage(request):
-    context = {
-        "students": {
+def homepage_view(request):
+    students = {
             "Ethan Ward" : Student("Ethan Ward", "I'm 18 years old. I'm Taken. Favorite Color is Purple", "Grunge", "To land a job in Cspire and grow my music", "Dishwasher at Corner Store", "Bruce, MS"),
 
             "Rylee Chisholm" : Student("Rylee Chisholm", "5'5 and clingy, but thats okay.", "Pop and anything that sounds terrible.", "To be 6ft tall.  I did dream once about this really weird fun house with a serial killer dressed as a neon pumpkin.", "Hometown Pizza delivery driver and cook", "Mom"),
@@ -156,13 +155,19 @@ def homepage(request):
             "Ryan Bennett" : Student("Ryan Bennett", "I'm a boring guy who plays video games.", "Classic Country", "Become a video game developer", "Server/Delivery Driver", "Saltillo, Mississippi"),
 
             "LOGAN T WILKINS" : Student("LOGAN T WILKINS", "I love video games and play a large variety of them my favorites being soul calibur, halo, for honor, skyrim and fallout 4. I enjoy various forms of art as any form of expressing creativity brings me joy, I like to draw, create ships in space engineers, build models in blender, paint, write, make characters in video games, build maps in halo, etc. I also love to spar, as in sword fighting or other weapons fighting and i have practiced many martial arts to improve my sword fighting capability. Some of the martial arts i have studied involve ", "Alternative rock is my top favorite, then there's instrumental, and dubstep... I also like christian rock", "All my life i've wanted a role in creating video game, and it aligns with many of my favorite things to do, how ever I have to one day open a HEMA academy to teach sword play and martial arts", "I currently work at Larsons Cashsaver Water Valley MS", "Memphis")}
-    }
-    form = ProfileForm(request.GET)
+    
+    form = SearchForm(request.GET)
     if form.is_valid():
         name = form.cleaned_data["name"]
-        
-
-        return render(request, "home.html",context,  {"form":form, "name":name})
+        for name2, info in students.items():
+            if name2 == name:
+                the_name = info.u_name
+                bio = info.bio
+                fav_genre = info.fav_genre
+                dream = info.dream
+                job = info.job
+                hometown = info.hometown
+        return render(request, "home.html", {"the_name":the_name, "bio":bio, "fav_genre":fav_genre, "dream":dream, "job":job, "hometown":hometown})
             
     else:
         return render(request,"home.html", {"form":form})
