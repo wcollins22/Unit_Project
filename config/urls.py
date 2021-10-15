9d053f02816595480e07bf840d756e71267469b9
JustinAshmore22 <88673704+JustinAshmore22@users.noreply.github.com>
2021-10-15 08:50:57 -0500

    d

--- config/urls.py
+++ config/urls.py
@@ -8,13 +8,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("profile/", app.views.profile_function, name="pf"),
-<<<<<<< HEAD
-    path("person/<u_name>/", app.views.page_view, name="person"),
-    path("home/", app.views.home_view, name="home"),
+    path("person/<name>/", app.views.page_view, name="person"),
+    path("homepage/", app.views.homepage_view, name="home"),
     
-=======
-    path("homepage/", app.views.homepage_view, name="hpnew"),
-    path("homepage/<name>/", app.views.page_view, name="page")
->>>>>>> 33583434129e181da1c65ed0820a13be925c450a
 ]
 urlpatterns += staticfiles_urlpatterns()
