1e45170c757ed3484b38b919cb03a4940c3dfe37
LoganColey <lcoley22@basecampcodingacademy.org>
2021-10-15 09:02:28 -0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,8 +8,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("profile/", app.views.profile_function, name="pf"),
+<<<<<<< Updated upstream
     path("person/<name>/", app.views.page_view, name="person"),
     path("homepage/", app.views.homepage_view, name="home"),
+=======
+    path("homepage/<name>/", app.views.page_view, name="person"),
+    path("home/", app.views.homepage_view, name="hpnew"),
+>>>>>>> Stashed changes
     
 ]
 urlpatterns += staticfiles_urlpatterns()
